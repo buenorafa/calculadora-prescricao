9ff12d7904696c3c38de7e901a242840786f8de9
buenorafa <rafaelbuenooo@outlook.com>
2025-05-28 21:10:43 -0300

    16610-formulario: adiciona o provider da Calculadora

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "./layout-base/layout";
 import CalculadoraForm from "./calculadora-form";
-import {Home} from "./home-page/home";
+import { Home } from "./home-page/home";
+import { CalculoPrescricaoProvider } from "./context/calculo-prescricao-context";
 
 function App() {
   return (
@@ -9,7 +10,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/calculadora" element={<CalculadoraForm />} />
+          <Route
+            path="/calculadora"
+            element={
+              <CalculoPrescricaoProvider>
+                <CalculadoraForm />
+              </CalculoPrescricaoProvider>
+            }
+          />
           {/* Adicione outras rotas aqui */}
         </Route>
       </Routes>
