ddafe68643d40cada7476397107d191061b52871
buenorafa <rafaelbuenooo@outlook.com>
2025-05-28 21:20:15 -0300

    16610-formulario: Mostra o multistepform

--- src/calculadora-form/index.tsx
+++ src/calculadora-form/index.tsx
@@ -1,12 +1,49 @@
-import { Form } from "@/components/ui/form";
+import { useState } from "react";
 import { DadosGeraisForm } from "./components/dados-gerais";
+import { PretensaoAbstratoForm } from "./components/pretensao-abstrato";
+import { PretensaoConcretoForm } from "./components/pretensao-concreto";
+import { DatasESuspensoesForm } from "./components/datas-e-suspensoes";
+import { useCalculoPrescricao } from "../context/calculo-prescricao-context";
+import { DadosOperadorForm } from "./components/dados-operador";
 
-function CalculadoraForm() {
+export default function CalculoPrescricaoIndex() {
+  const [step, setStep] = useState(0);
+  const { dados } = useCalculoPrescricao();
+
+  const goToNextStep = () => setStep((prev) => prev + 1);
+  const goToPreviousStep = () => setStep((prev) => prev - 1);
+
   return (
-    <div className="w-1/2 mx-auto self-center">
-      <DadosGeraisForm></DadosGeraisForm>
+    <div className="max-w-3xl mx-auto">
+      {step === 0 && <DadosGeraisForm onNext={goToNextStep} />}
+
+      {step === 1 && dados.tipoPrescricao === "em-abstrato" && (
+        <PretensaoAbstratoForm
+          onNext={goToNextStep}
+          onBack={goToPreviousStep}
+        />
+      )}
+
+      {step === 1 && dados.tipoPrescricao === "em-concreto" && (
+        <PretensaoConcretoForm
+          onNext={goToNextStep}
+          onBack={goToPreviousStep}
+        />
+      )}
+
+      {step === 2 && (
+        <DatasESuspensoesForm onBack={goToPreviousStep} onNext={goToNextStep} />
+      )}
+
+      {step === 3 && (
+        <DadosOperadorForm
+          onBack={goToPreviousStep}
+          onNext={() => {
+            // Aqui você pode substituir pelo envio final
+            console.log(dados);
+          }}
+        />
+      )}
     </div>
   );
 }
-
-export default CalculadoraForm;
