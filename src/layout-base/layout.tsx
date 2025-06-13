8eda176a430b399dbaa4430d726f3a36c90f3a69
islanpedro01 <islanpedro.12@hotmail.com>
2025-06-13 11:50:39 -0300

    nav ajustado

--- src/layout-base/layout.tsx
+++ src/layout-base/layout.tsx
@@ -5,28 +5,23 @@ import { Link } from "react-router-dom";
 export function Layout() {
   return (
     <div className="layout-base h-screen">
-      <main className="layout-content flex h-full">
-        <aside className="flex h-full flex-col w-1/3 p-4 gap-62 bg-[#ADB6A7]">
+      <main className="layout-content flex flex-col h-full">
+        <nav className="flex w-full flex-row h-24 p-4 justify-between bg-white">
           <div className="flex justify-between items-center">
               <Link to="/"><img src="/CP.svg" alt="Logo Calculadora de prescrição" /></Link>
-            <Link to ="#" className="h-10 flex font-semibold text-white w-2/6 bg-[#2B2823] items-center justify-center rounded-md hover:bg-[#685c49]">
+          </div>
+          <div className="w-1/6 h-2/3 flex flex-row text-center self-center text-lg justify-between"> 
+            <Link to ="#"
+              className="flex font-semibold p-2 text-[#2B2823] solid bg-white justify-center items-center rounded-md hover:bg-[#2B2823] hover:text-white"
+            >
+              Consultar Histórico
+            </Link>
+             <Link to ="#" className="flex p-2 font-semibold text-white bg-[#2B2823] items-center justify-center rounded-md hover:bg-[#685c49]">
                 Criar conta
             </Link>
           </div>
-          <div className="h-2/5 w-4/6 flex flex-col text-center self-center text-2xl gap-24">
-            <Link to="/type-selector"
-              className="h-full font-semibold flex text-[#2B2823] bg-[#9AA394] justify-center items-center rounded-md hover:bg-[#2B2823] hover:text-white"
-            >
-              Calcular Prescrição
-            </Link>
-            <a
-              href="#"
-              className="h-full flex font-semibold text-[#2B2823] bg-[#9AA394] justify-center items-center rounded-md hover:bg-[#2B2823] hover:text-white"
-            >
-              Consultar Histórico
-            </a>
-          </div>
-        </aside>
+          
+        </nav>
         <div className="flex h-full w-full">
             <Outlet />
         </div>
