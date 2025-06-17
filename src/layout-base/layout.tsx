e458f0853bf90c8514939016ad33dff8d26fffc5
islanpedro01 <islanpedro.12@hotmail.com>
2025-06-17 12:26:54 -0300

    Logo ajustada

--- src/layout-base/layout.tsx
+++ src/layout-base/layout.tsx
@@ -8,7 +8,7 @@ export function Layout() {
       <main className="layout-content flex flex-col w-full h-full">
         <nav className="flex flex-row h-24 justify-between items-center py-4 m-auto w-5/7 bg-white">
         
-              <Link to="/" className="text-start"><img src="/CP.svg" alt="Logo Calculadora de prescrição" /></Link>
+              <Link to="/" className="text-start w-28"><img src="/Logotipo CPP.svg" alt="Logo Calculadora de prescrição" /></Link>
 
           <div className="w-2/6 h-2/3 flex flex-row text-center self-center text-lg justify-end gap-10"> 
             <Link to ="#"
