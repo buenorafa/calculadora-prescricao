5719feafb424c0a55c7bb223e294c74f8245fdc5
buenorafa <rafaelbuenooo@outlook.com>
2025-06-25 23:40:01 -0300

    18598: adiciona os formulários para os novos tipos de prescricao

---
+++ src/calculadora-form/components/index.tsx
@@ -0,0 +1,6 @@
+export { DadosGeraisForm } from "./dados-gerais";
+export { FormAbstrata } from "./form-abstrata";
+export { FormExecutoria } from "./form-executoria";
+export { FormRetroativa } from "./form-retroativa";
+export { FormIntercorrente } from "./form-intercorrente";
+export { DadosOperadorForm } from "./dados-operador";
--- src/calculadora-form/index.tsx
+++ src/calculadora-form/index.tsx
@@ -1,43 +1,61 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom"; // ✅ Importar useNavigate
-import { DadosGeraisForm } from "./components/dados-gerais";
+import { useNavigate } from "react-router-dom";
+
 import { useCalculoPrescricao } from "../context/calculo-prescricao-context";
-import { DadosOperadorForm } from "./components/dados-operador";
+
 import { postCalculoPrescricao } from "@/service/api";
-import { DatasProcessoForm } from "./components/datas-processo";
-import { CausasInterrupcaoForm } from "./components/causas-interrupcao";
+
+import {
+  DadosGeraisForm,
+  FormAbstrata,
+  FormExecutoria,
+  FormRetroativa,
+  FormIntercorrente,
+  DadosOperadorForm,
+} from "./components/";
 
 export default function CalculoPrescricaoIndex() {
   const [step, setStep] = useState(0);
-  const { dados, setResultado } = useCalculoPrescricao(); // ✅ Importar setResultado do contexto
-  const navigate = useNavigate(); // ✅ Inicializar hook de navegação
+  const { dados, setResultado } = useCalculoPrescricao();
+  const navigate = useNavigate();
 
   const goToNextStep = () => setStep((prev) => prev + 1);
   const goToPreviousStep = () => setStep((prev) => prev - 1);
 
+  const formComponentMap = {
+    ABSTRATA: <FormAbstrata onBack={goToPreviousStep} onNext={goToNextStep} />,
+    CONCRETO: (
+      <FormExecutoria onBack={goToPreviousStep} onNext={goToNextStep} />
+    ),
+    RETROATIVA: (
+      <FormRetroativa onBack={goToPreviousStep} onNext={goToNextStep} />
+    ),
+    INTERCORRENTE: (
+      <FormIntercorrente onBack={goToPreviousStep} onNext={goToNextStep} />
+    ),
+  } as const;
+
+  // Normaliza a chave e valida
+  const tipo = dados.tipoPrescricao?.toUpperCase();
+  const isTipoValido = tipo && tipo in formComponentMap;
+
   return (
     <div className="max-w-3xl mx-auto">
       {step === 0 && <DadosGeraisForm onNext={goToNextStep} />}
+      {/* {step === 0 && <FormExecutoria onNext={goToNextStep} />} */}
 
-      {step === 1 && (
-        <DatasProcessoForm onNext={goToNextStep} onBack={goToPreviousStep} />
-      )}
+      {step === 1 &&
+        isTipoValido &&
+        formComponentMap[tipo as keyof typeof formComponentMap]}
 
       {step === 2 && (
-        <CausasInterrupcaoForm
-          onBack={goToPreviousStep}
-          onNext={goToNextStep}
-        />
-      )}
-
-      {step === 3 && (
         <DadosOperadorForm
           onBack={goToPreviousStep}
           onNext={async () => {
             try {
               const response = await postCalculoPrescricao(dados);
-              setResultado(response); // ✅ Armazena o resultado no contexto
-              navigate("/result"); // ✅ Navega para a página de resultado
+              setResultado(response);
+              navigate("/result");
             } catch (error) {
               console.error("❌ Erro ao gerar payload:", error);
             }
